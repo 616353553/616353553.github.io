bcb21545c8a8626dce7ec970201a965ba2455d34
616353553 <616353553@qq.com>
2017-12-04 19:33:03 -0600

    update

--- home.js
+++ home.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 });
 
 function viewDetail(item_data) {
-    window.location.replace("http://616353553.github.io/detail.html");
+    window.location.replace("https://616353553.github.io/detail.html");
 }
 
 function addToCart(item_data) {
