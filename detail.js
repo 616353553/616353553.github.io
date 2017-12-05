add039c47f5587c2801437ffa0a4d4b150da148e
616353553 <616353553@qq.com>
2017-12-05 01:03:57 -0600

    update

--- detail.js
+++ detail.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
-    console.log("hello");
     var item_data = localStorage.getItem("item_data");
-    console.log(item_data);
+    $.each(item_data, function(key, value){
+        if(key === "images") {
+            $.each(value, function(index, img_dir){
+                var li_element = $("<li>").append($("<img>", {"id": "itemImage" + index, "src": img_dir}));
+                $("#slider").append(li_element);
+            });
+        } else {
+            var title = key.charAt(0).toUpperCase() + key.slice(1);
+            $("#main").append($("<h3>", {"text": title + ": " + value}));
+        }
+    });
+    $("#main").append($("<input>", {"type": "button", "name": "Add to cart", "id": "add", "value": "Add to cart"}));
+    $("#add").click(function(){
+        //addToCart(value);
+    });
+
+    $("#slider").bxSlider({
+        auto: false,
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1,
+        slideWidth: 600,
+        slideMargin: 10,
+        randomStart: false,
+        pagerType: "short"
+    });
 });
